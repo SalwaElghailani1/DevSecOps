76028b4187c00aa95956a0ca7566dba33d1f6205
SqalwaElghailani <salwaelghailani8@gmail.com>
2025-12-17 17:53:04 +0100

    test jenkins

--- src/main/java/faculte/devsecops/web/APIRestfull.java
+++ src/main/java/faculte/devsecops/web/APIRestfull.java
@@ -2,10 +2,17 @@ package faculte.devsecops.web;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class APIRestfull {
     @GetMapping("/hello")
     public String hello() {
+
         return "Hello DevOps 🚀 Spring Boot is running!";
+    }
+    @GetMapping("/test")
+    public String test() {
+
+        return "test jenkins !!!";
     }
 }
