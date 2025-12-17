8f5b488fb852e663bcf517be2f16006069e99966
SqalwaElghailani <salwaelghailani8@gmail.com>
2025-12-17 04:57:15 +0100

    Initial Spring Boot project

---
+++ src/main/java/faculte/devsecops/web/APIRestfull.java
@@ -0,0 +1,11 @@
+package faculte.devsecops.web;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class APIRestfull {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello DevOps 🚀 Spring Boot is running!";
+    }
+}
